f1ea216264dea6312e08bc17c17e313012490a0b
Trenton Stiles <stiles.trenton@gmail.com>
2023-07-25 19:26:57 -0500

    removed redundant comments, added more data for test cases

--- gvote/cmd_parser.py
+++ gvote/cmd_parser.py
@@ -1,36 +1,12 @@
 
-# example : !!gvote is jason gay? yes no maybe
+# example : !!gvote is jason a good person? yes no maybe
 
 def parse(cmd: str) -> tuple:
-    ''' 
-    Gets the command the user puts in and returns the prompt and a 
-    list of options to vote on.
-
-    This function takes one str, 'cmd'. Returns a tuple (str, list(str)).
-
-    Parameters:
-        cmd (str): discord message prompting the bot
-
-    Returns:
-        tuple: (str, list(str))
-    '''
     prompt = _get_prompt(cmd)
     opts = _get_options(cmd)
     return (prompt, opts)
 
 def _get_prompt(cmd: str) -> str:
-    ''' 
-    Gets the command the user puts in and returns the question they asked.
-
-    This function takes one str, 'cmd'. Returns a str of the prompt.
-
-    Parameters:
-        cmd (str): discord message prompting the bot
-
-    Returns:
-        str: The str of the prompt
-    '''
-
     # '!!gvote ' is 8 chars long so prompt index is @ 8.
     prompt_start = 8 
     # finds the last index of the ? which indicates the end of prompt
@@ -39,18 +15,6 @@ def _get_prompt(cmd: str) -> str:
     return cmd[prompt_start:prompt_end]
 
 def _get_options(cmd: str) -> list:
-    ''' 
-    Gets the command the user puts in and returns the options a user can vote on.
-
-    This function takes one str, 'cmd'. Returns a list of str.
-
-    Parameters:
-        cmd (str): discord message prompting the bot
-
-    Returns:
-        list: The list of str a user will vote on
-    '''
-
     # starting index after the last question mark assuming thats the prompt
     opt_string_start = cmd.rfind('?') + 1
     # a str from the last '?' chr to the end of the str
--- tests/test_cmd_parser.py
+++ tests/test_cmd_parser.py
@@ -1,7 +1,42 @@
+import pytest
 from gvote import cmd_parser
 
-good_prompts = []
-bad_prompts = []
+class Test_Data:
+    def __init__(self, command, prompt=None, options=None) -> None:
+        self.command = command
+        self.prompt = prompt
+        self.options = options
+
+good_tests = []
+bad_tests = []
+
+# no leading space between question chr
+good_tests += Test_Data('!!gvote how do we know?yes no maybe',
+                        'how do we know?', 
+                        ['yes', 'no', 'maybe'])
+# extra spaces
+good_tests += Test_Data('!!gvote  are   we  blasting  ?    yes  no  maybe ',
+                        'are we blasting?',
+                        ['yes', 'no', 'maybe'])
+# extra question marks
+good_tests += Test_Data('!!gvote are we blasting? yes? no? maybe?',
+                        'are we blasting?',
+                        ['yes?', 'no?', 'maybe?'])
+
+# no question marks
+bad_tests += Test_Data('!!gvote how do we know yes no maybe',
+                       None,
+                       None)
+
+# no options
+bad_tests += Test_Data('!!gvote how do we know?',
+                       None,
+                       None)
+
+# only question marks
+bad_tests += Test_Data('!!gvote ? ? ?',
+                       None,
+                       None)
 
 def test_get_prompt():
     pass
