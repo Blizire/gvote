b1f1f2db4aa681440fa42149b5a06f4764d7cd16
Trenton Stiles <stiles.trenton@gmail.com>
2023-08-09 00:02:05 -0500

    fixed parsing strings with multiple spaces + question marks

--- gvote/cmd_parser.py
+++ gvote/cmd_parser.py
@@ -7,23 +7,42 @@ def parse(cmd: str) -> tuple:
     return (prompt, opts)
 
 def _get_prompt(cmd: str) -> str:
+    ''' Obtains the text between '!!gvote ' and the first question mark '''
+
     # '!!gvote ' is 8 chars long so prompt index is @ 8.
     prompt_start = 8 
     # finds the last index of the ? which indicates the end of prompt
     # +1 to include the question mark.
-    prompt_end = cmd.rfind('?') + 1
+    prompt_end = cmd.find('?') + 1
     prompt = cmd[prompt_start:prompt_end]
     # ensure no extra spaces are in it
     prompt = prompt.split()
     prompt = ' '.join(prompt)
-    # ensure theres a space between the question mark and prompt
+    # ensure space between last word and question mark
+    if prompt[-2] != ' ':
+        prompt = prompt[:-1] + ' ?'    
     return prompt
 
 def _get_options(cmd: str) -> list:
-    # starting index after the last question mark assuming thats the prompt
-    opt_string_start = cmd.rfind('?') + 1
+    ''' recieves a 'cmd' and we parse it after the 'prompt' and get the 'options' afterwards, each
+     option will be seperated by spaces '''
+    
+    # assumes prompt is correct and has one question mark so after the first question mark
+    # will be the listed options seperated by spaces. we split the spaces and return those options.
+    opt_string_start = cmd.find('?') + 1
     # a str from the last '?' chr to the end of the str
-    option_string = cmd[opt_string_start::].strip(' ')
+    option_string = cmd[opt_string_start::].strip()
     # breaks the options up assuming they are spaced correctly
-    options = option_string.split(' ')
+    options = option_string.split()
+    # removes any extra spaces that may have been inputed
+    for i,opt in enumerate(options):
+        if opt == ' ':
+            options[i].remove()
+
     return options
+
+if __name__ == '__main__':
+    s = '!!gvote are we blasting? yes? no? maybe?'
+    resp = _get_prompt(s)
+    resp = _get_options(s)
+    print(resp)
--- tests/test_cmd_parser.py
+++ tests/test_cmd_parser.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pytest
 import gvote.cmd_parser
 
-class Test_Data:
+class cmd:
     def __init__(self, command, prompt=None, options=None) -> None:
         self.command = command
         self.prompt = prompt
@@ -15,30 +15,30 @@ good_tests = []
 bad_tests = []
 
 # no leading space between question chr
-good_tests.append(Test_Data('!!gvote how do we know?yes no maybe',
-                        'how do we know?', 
+good_tests.append(cmd('!!gvote how do we know?yes no maybe',
+                        'how do we know ?', 
                         ['yes', 'no', 'maybe']))
 # extra spaces
-good_tests.append(Test_Data('!!gvote  are   we  blasting  ?    yes  no  maybe ',
-                        'are we blasting?',
+good_tests.append(cmd('!!gvote  are   we  blasting  ?    yes  no  maybe ',
+                        'are we blasting ?',
                         ['yes', 'no', 'maybe']))
 # extra question marks
-good_tests.append(Test_Data('!!gvote are we blasting? yes? no? maybe?',
-                        'are we blasting?',
+good_tests.append(cmd('!!gvote are we blasting? yes? no? maybe?',
+                        'are we blasting ?',
                         ['yes?', 'no?', 'maybe?']))
 
 # no question marks
-bad_tests.append(Test_Data('!!gvote how do we know yes no maybe',
+bad_tests.append(cmd('!!gvote how do we know yes no maybe',
                        None,
                        None))
 
 # no options
-bad_tests.append(Test_Data('!!gvote how do we know?',
+bad_tests.append(cmd('!!gvote how do we know?',
                        None,
                        None))
 
 # only question marks
-bad_tests.append(Test_Data('!!gvote ? ? ?',
+bad_tests.append(cmd('!!gvote ? ? ?',
                        None,
                        None))
 
