63b98aeb7c890ae8d3c652bacb73883ef466508b
Trenton Stiles <stiles.trenton@gmail.com>
2023-08-10 01:31:42 -0500

    bug fixes, added tests for checking bad cases

--- gvote/command_parser.py
+++ gvote/command_parser.py
@@ -11,6 +11,10 @@ class Command:
             return None
         self.prompt = self._get_prompt(self.command)
         self.options = self._get_options(self.command)
+
+        if self.prompt == None or self.options == None:
+            raise CommandParseError("Could not parse one of the paramters.")
+
         return (self.prompt, self.options)
 
     def _get_prompt(self, cmd: str) -> str:
@@ -21,6 +25,15 @@ class Command:
         # finds the last index of the ? which indicates the end of prompt
         # +1 to include the question mark.
         prompt_end = cmd.find('?') + 1
+
+        # if theres no questeion mark then the end will be 0 before the start
+        if prompt_end <= prompt_start:
+            raise CommandParseError("Could not find the end of prompt.")
+        
+        # if the start and end is 1 then its just a question mark.
+        if (prompt_end - prompt_start) <= 1:
+            raise CommandParseError("Invalid prompt.")
+
         prompt = cmd[prompt_start:prompt_end]
 
         # ensure no extra spaces are in it
@@ -39,6 +52,10 @@ class Command:
         # first index after finding the first question mark
         opt_string_start = cmd.find('?') + 1
 
+        # check if theres any input ( no options after )
+        if opt_string_start >= len(cmd):
+            raise CommandParseError("Could not find options to parse after prompt.")
+
         # a str from the last '?' chr to the end of the str
         option_string = cmd[opt_string_start::].strip()
 
@@ -51,3 +68,11 @@ class Command:
                 options[i].remove()
 
         return options
+    
+class CommandParseError(Exception):
+    pass
+
+if __name__ == '__main__':
+    prompt, options = Command('!!gvote ?').parse()
+    print(prompt)
+    print(options)
--- tests/test_command_parser.py
+++ tests/test_command_parser.py
@@ -3,10 +3,9 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pytest
-from gvote.command_parser import Command 
+from gvote.command_parser import Command, CommandParseError
 
 good_tests = []
-bad_tests = []
 
 # no leading space between question chr
 good_tests.append(Command('!!gvote how do we know?yes no maybe',
@@ -21,26 +20,31 @@ good_tests.append(Command('!!gvote are we blasting? yes? no? maybe?',
                         'are we blasting ?',
                         ['yes?', 'no?', 'maybe?']))
 
-# no question marks
-bad_tests.append(Command('!!gvote how do we know yes no maybe',
-                       None,
-                       None))
-
-# no options
-bad_tests.append(Command('!!gvote how do we know?',
-                       None,
-                       None))
-
-# only question marks
-bad_tests.append(Command('!!gvote ? ? ?',
-                       None,
-                       None))
-
+# In hindsight this is actually a bad way to test. Should've been individual tests for each case.
 def test_parse():
     for data in good_tests:
         prompt, opts = Command(data.command).parse()
         assert prompt == data.prompt
         assert opts == data.options
 
-def test_bad_parse():
-    pass
+
+def test_no_question_marks():
+        ''' test handling for no question marks '''
+        with pytest.raises(CommandParseError) as e:
+            prompt, opts = Command('!!gvote how do we know yes no maybe').parse()
+
+        assert str(e.value) == "Could not find the end of prompt."
+
+def test_prompt_with_no_options():
+        ''' test handling for a prompt with no options '''
+        with pytest.raises(CommandParseError) as e:
+            prompt, opts = Command('!!gvote how do we know?').parse()
+
+        assert str(e.value) == "Could not find options to parse after prompt."
+
+def test_prompt_with_only_question_marks():
+        ''' test handling for a prompt with only question marks '''
+        with pytest.raises(CommandParseError) as e:
+            prompt, opts = Command('!!gvote ? ? ?').parse()
+
+        assert str(e.value) == "Invalid prompt."
